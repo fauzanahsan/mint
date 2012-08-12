16419762bce5544035a0d9902275e6cb1f19fb4e
Fauzan Ahsan <fauzanahsan@Fauzans-MacBook-Air.local>
2012-08-12 13:21:36 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mint::Application.config.secret_token = '617650a69b4d4915a580dd1074b185e0e45885d4f560eefd42393a223e674a98ff5c4613588e1cc8019a09807909f87ba3ac94a5821bf5fbd51bcb6545102235'
